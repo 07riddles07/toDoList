8e36a07d37a37f09a295e9d322e779492bca9563
Anastasia Rarova <07tangled07@gmail.com>
2022-02-07 05:21:51 +0300

    To Do List project created

---
+++ script.js
@@ -0,0 +1,52 @@
+const inputField = document.querySelector(".inputField");
+const btn = document.querySelector(".btn");
+const toDoContainer = document.querySelector(".toDoContainer");
+
+btn.addEventListener("click", () => {
+  const item = document.createElement("li");
+  item.innerText = inputField.value;
+  if (inputField.value.length === 0) {
+    return false;
+    }
+  item.classList.add("toDoAdded");
+  toDoContainer.appendChild(item);
+  inputField.value = "";
+
+  item.addEventListener("click", () => {
+    item.classList.add("toDoCompleted");
+  });
+
+  item.addEventListener("dblclick", () => {
+    toDoContainer.removeChild(item);
+  });
+
+});
+
+inputField.addEventListener('keyup', (e) => {
+  if(e.keyCode===13) {
+    const item = document.createElement("li");
+    item.innerText = inputField.value;
+    if (inputField.value.length === 0) {
+      return false;
+      }
+    item.classList.add("toDoAdded");
+    toDoContainer.appendChild(item);
+    inputField.value = "";
+
+    item.addEventListener("click", () => {
+      item.classList.add("toDoCompleted");
+    });
+  
+    item.addEventListener("dblclick", () => {
+      toDoContainer.removeChild(item);
+    });
+  }
+})
+
+
+
+
+
+
+
+
